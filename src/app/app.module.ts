27b0257ffbb7625353ddf20bf7cc55050120a82e
ramon <doutoramon@gmail.com>
2023-01-11 16:17:52 -0300

    incluindo components de visualizacao e os modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './container/home/home.component';
+import { AboutComponent } from './container/about/about.component';
+import { ContactComponent } from './container/contact/contact.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
